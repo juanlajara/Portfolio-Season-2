5f9471ac707e1fabd641c68ecee7892e26fd7b99
juanlajara <juanlajara001@gmail.com>
2020-08-14 00:31:35 -0500

    Added more projects

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,34 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Footer extends Component {
-  render() {
+	render() {
+		if (this.props.data) {
+			var networks = this.props.data.social.map(function (network) {
+				return (
+					<li key={network.name}>
+						<a href={network.url}>
+							<i className={network.className}></i>
+						</a>
+					</li>
+				);
+			});
+		}
 
-    if(this.props.data){
-      var networks= this.props.data.social.map(function(network){
-        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
-      })
-    }
+		return (
+			<footer>
+				<div className="row">
+					<div className="twelve columns">
+						<ul className="social-links">{networks}</ul>
 
-    return (
-      <footer>
-
-     <div className="row">
-        <div className="twelve columns">
-           <ul className="social-links">
-              {networks}
-           </ul>
-
-           <ul className="copyright">
-              <li>&copy; Copyright 2017 Nordic Giant</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
-           </ul>
-
-        </div>
-        <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
-     </div>
-  </footer>
-    );
-  }
+						<ul className="copyright">
+							<li>&copy; Andres Lajara</li>
+							<li>
+								Design by{" "}
+								<a title="Styleshout" href="http://www.styleshout.com/">
+									Dre
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div id="go-top">
+						<a className="smoothscroll" title="Back to Top" href="#home">
+							<i className="icon-up-open"></i>
+						</a>
+					</div>
+				</div>
+			</footer>
+		);
+	}
 }
 
 export default Footer;
